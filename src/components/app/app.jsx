3012b28268bd96b88381c124c562785b03c82900
Denis Latyshev <d.d.latyshev@yandex.ru>
2023-07-29 11:26:49 +0300

    добавил функционал по загрузке информации из API

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,27 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import styles from "./app.module.css";
-import { data } from "../../utils/data";
 import AppHeader from "../AppHeader/app_header";
 import BurgerIngredients from '../BurgerIngredients/burger_ingredients';
 import BurgerConstructor from '../BurgerConstructor/burger_constructor';
+import { getIngredience } from '../../utils/api';
 
 function App() {
+
+  const [ingredinces, setIngredience] = useState([]);
+
+  useEffect(() => {
+    getIngredience()
+      .then(res => setIngredience([...res.data]))
+  }, [])
+
   return (
     <>
       <AppHeader/>
       <main className={styles.main}>
-        <BurgerIngredients data={data}/>
-        <BurgerConstructor data={data}/> 
+        <BurgerIngredients data={ingredinces}/>
+        <BurgerConstructor data={ingredinces}/> 
       </main>
     </>
   );
