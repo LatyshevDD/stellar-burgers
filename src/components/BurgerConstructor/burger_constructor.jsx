028c1f45d887852192548f212c9024d0fc161f9f
Denis Latyshev <d.d.latyshev@yandex.ru>
2023-08-22 14:38:04 +0300

    Добавлен функционал добавления булок и ингридиентов через useReducer

--- src/components/BurgerConstructor/burger_constructor.jsx
+++ src/components/BurgerConstructor/burger_constructor.jsx
@@ -1,66 +1,80 @@
-import React, { useContext } from "react";
-import { useMemo } from "react";
+import React, { useContext, useMemo } from "react";
 import styles from './burger_constructor.module.css';
 import { DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
-import { ingredientsPropType } from '../../utils/prop-types';
 import { ConstructorContext } from '../../services/constructorContext';
 
 export default function BurgerConstructor({onOpenModal}) {
 
   const data = useContext(ConstructorContext);
 
-  const ingredients = React.useMemo(() => data.filter(item => item.type == "sauce" || item.type == "main"), [data]);
-  const bun = React.useMemo(() => data.filter(item => item.type == "bun"), [data]);
-  const price = React.useMemo(() => ingredients.reduce((sum, item) => {return sum + item.price}, 0) + bun.reduce((sum, item) => {return sum + item.price}, 0), [data]);
+  const ingredients = React.useMemo(() => data.ingredients, [data]);
+
+  const bun = React.useMemo(() => data.bun, [data]);
+  
+  const totalPrice = React.useMemo(() => {
+    let ingrediencePrice = 0;
+    let bunPrice = 0;
+    if (ingredients) {
+      ingrediencePrice = ingredients.reduce((sum, item) => {return sum + item.price}, 0)
+    }
+    if (bun) {
+      bunPrice = bun.price * 2;
+    }
+    return ingrediencePrice + bunPrice;
+  },
+    [ingredients, bun]
+  );
   
   return (
     <section className={`${styles.section} mt-25`}>
-      {
-        bun.length != 0 && (
-            <ConstructorElement
-              type="top"
-              text={bun[0].name + ' ' + '(верх)'}
-              price={bun[0].price}
-              thumbnail={bun[0].image}
-              isLocked='true'
-              extraClass='ml-8 mr-2'
-            />
-        )
-      }
-      <ul className={`${styles.ingredients} custom-scroll`}> 
-      {
-        ingredients.map(item => (
-          <li className={`${styles.ingredient} mr-2`} key={item._id}>
-            <DragIcon type="primary" />
-            <ConstructorElement
-              text={item.name}
-              price={item.price}
-              thumbnail={item.image}
-            />
-          </li>
-        ))
-      }
-      </ul> 
-      {
-        bun.length != 0 && (
-            <ConstructorElement
-              type="bottom"
-              text={bun[1].name + ' ' + '(низ)'}
-              price={bun[1].price}
-              thumbnail={bun[1].image}
-              isLocked='true'
-              extraClass='ml-8 mr-2'
-            />
-        )
-      }  
+      <div className={styles.constructor}>
+        {
+          bun && (
+              <ConstructorElement
+                type="top"
+                text={bun.name + ' ' + '(верх)'}
+                price={bun.price}
+                thumbnail={bun.image}
+                isLocked='true'
+                extraClass='ml-8 mr-2'
+              />
+          )
+        }
+        <ul className={`${styles.ingredients} custom-scroll`}> 
+        {
+          ingredients.map((item, index) => (
+            <li className={`${styles.ingredient} mr-2`} key={index}>
+              <DragIcon type="primary" />
+              <ConstructorElement
+                text={item.name}
+                price={item.price}
+                thumbnail={item.image}
+              />
+            </li>
+          ))
+        }
+        </ul> 
+        {
+          bun && (
+              <ConstructorElement
+                type="bottom"
+                text={bun.name + ' ' + '(низ)'}
+                price={bun.price}
+                thumbnail={bun.image}
+                isLocked='true'
+                extraClass='ml-8 mr-2'
+              />
+          )
+        }  
+      </div>  
       <div className={`${styles.order} mt-10`}>
         <div className={styles.price}>
           <p className="text text_type_digits-default">
-            {price}
+            {totalPrice}
           </p>
           <CurrencyIcon type="primary" />
         </div>
