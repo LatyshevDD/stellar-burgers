033485c82f7370e7b7ee0c5b7dd051b1881dce32
Denis Latyshev <d.d.latyshev@yandex.ru>
2023-09-25 10:42:58 +0300

    Настроил изменение цвета NavLink в AppHeader

--- src/components/AppHeader/app_header.jsx
+++ src/components/AppHeader/app_header.jsx
@@ -3,38 +3,50 @@ import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
 import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './app_header.module.css'
-import { Link, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
 
 export default function AppHeader() {
   return (
     <>
       <header className={styles.header}>
       <nav className={`${styles.nav} mt-4 mb-4`}>
-        <Link to="/" style={{textDecoration: "none", color: "rgba(242,242,243,1)"}}>
+        <NavLink 
+          to="/"
+          className={({ isActive}) => isActive ? styles.active : styles.pending}
+          end
+        >
           <div className={`${styles.link_constructor} ml-5 mr-5`}>
             <BurgerIcon type="primary" />
             <p className="text text_type_main-default ml-2">
               Конструктор
             </p>
           </div>
-        </Link>
-        <Link to="#" style={{textDecoration: "none", color: "rgba(133,133,173,1)"}}>
+        </NavLink>
+        <NavLink 
+          to="/feed"
+          className={({ isActive}) => isActive ? styles.active : styles.pending}
+          end
+        >
           <div className={`${styles.link_list} ml-5 ml-2`}>
-            <ListIcon type="secondary" />
-            <p className="text text_type_main-default text_color_inactive ml-2">
+            <ListIcon type="primary" />
+            <p className="text text_type_main-default ml-2">
               Лента заказов
             </p>
           </div>
-        </Link>
+        </NavLink>
         <Logo/>
-        <Link to="/login" style={{textDecoration: "none"}}>
+        <NavLink 
+          to="/profile"
+          className={({ isActive}) => isActive ? styles.active : styles.pending}
+          end
+        >
           <div className={`${styles.link_lk} mr-5`}>
-            <ProfileIcon type="secondary" />
-            <p className="text text_type_main-default text_color_inactive ml-2">
+            <ProfileIcon type="primary" />
+            <p className="text text_type_main-default ml-2">
               Личный кабинет
             </p>
           </div>
-        </Link>
+        </NavLink>
       </nav> 
       </header>
       <Outlet/>
