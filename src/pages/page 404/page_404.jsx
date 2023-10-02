6d482079ccacc87002b7d635afe75d99eb0aaddf
Denis Latyshev <d.d.latyshev@yandex.ru>
2023-10-02 15:38:46 +0300

    feat: на страницу 404 добавлена ссылка на главную страницу

--- src/pages/page 404/page_404.jsx
+++ src/pages/page 404/page_404.jsx
@@ -1,12 +1,29 @@
 import React from "react"
 import styles from './page_404.module.css'
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components"
+import { useNavigate } from "react-router-dom"
 
 export default function Page404() {
+
+  const navigate = useNavigate()
+
   return (
     <main className={styles.main}>
       <p className="text text_type_main-large">
-        404 Not found
+        Oops!
       </p>
+      <p className="text text_type_main-large">
+        Page not found
+      </p>
+      <Button 
+        htmlType="button" 
+        type="primary" 
+        size="small" 
+        extraClass="ml-2"
+        onClick={() => navigate('/')}
+      >
+        Вернуться на главную
+      </Button>
     </main>
   )
 }
