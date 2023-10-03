85728819cfd07a900d895de26b584b01bfb76e43
Denis Latyshev <d.d.latyshev@yandex.ru>
2023-10-03 16:21:51 +0300

    feat: настроена возможность деплоя на gh-pages, доработано отображение кнопок в profile_form

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import "./index.css"
 import reportWebVitals from "./reportWebVitals"
 import { store } from "./services/store"
 import { Provider } from 'react-redux'
-// import { router } from "./pages/router"
-// import { RouterProvider} from "react-router-dom";
 import App from "./components/App/app"
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App/>
       </BrowserRouter>
     </Provider>
