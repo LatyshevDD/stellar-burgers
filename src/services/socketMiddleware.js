381d6aa1f8747b483d6d686269f4bd13f74661a7
Denis Latyshev <d.d.latyshev@yandex.ru>
2023-10-13 20:43:20 +0300

    feat: добавить функцию переподключения сокета в socketMiddleware

--- src/services/socketMiddleware.js
+++ src/services/socketMiddleware.js
@@ -2,6 +2,8 @@ import { checkOrdersIngredients } from "../utils/utils"
 
 export const socketMiddleware = wsConfig => store => {
   let socket = null
+  let isConnected = false
+  let reconnectTimer = 0;
   const { onStart, onStop, onOpen, onMessage, onClose, onError } = wsConfig
 
   return next => action => {
@@ -10,10 +12,14 @@ export const socketMiddleware = wsConfig => store => {
     
     if (type === onStart) {
       socket = new WebSocket(payload)
+      isConnected = true
     }
     
     if(socket) {
       if (type === onStop) {
+        clearTimeout(reconnectTimer)
+        isConnected = false
+        reconnectTimer = 0
         socket.close(1000, 'Пользователь покинул страницу')
       }
 
@@ -22,6 +28,12 @@ export const socketMiddleware = wsConfig => store => {
       }
 
       socket.onclose = event => {
+        if (isConnected) {
+          reconnectTimer = setTimeout(() => {
+            dispatch({type: onStart});
+          }, 3000)
+        }
+
         if(event.wasClean) {
           dispatch(onClose('Closed correct'))
         } else {
